7e36083eddef03ffeee31062e44eb0f2e5cabddd
bitnaleeeee <bitnaleeeee@gmail.com>
2023-10-30 16:38:51 +0900

    메인페이지 레이아웃

---
+++ src/Question.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Question.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Question = () => {
+  return (
+    <div className="mainWrap">
+      <div className="mainNav">
+        <FontAwesomeIcon className="backIcon" icon={faChevronLeft} />
+        <FontAwesomeIcon className="searchIcon" icon={faBars} />
+        <FontAwesomeIcon className="searchIcon" icon={faMagnifyingGlass} />
+      </div>
+      <div className="contentsWrap">
+        <div className="contents">
+          <h1>Q1</h1>
+          <p>
+            새 학년 새 학기 수업 시간. 이 수업에 팀플이 있다는 걸 알게 되었다.
+            <br></br>
+            <span>이 때 내가 든 생각은?</span>
+          </p>
+          <div className="buttonWrap">
+            <button>
+              '새로운 사람 만나는 건 좋지'
+              <br></br>
+              <span>괜찮다고 생각한다</span>
+            </button>
+            <button>
+              '팀플 너무 싫어 드랍할 걸...'
+              <br></br>
+              <span>벌써부터 힘들다고 생각한다</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Question;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
+import Question from "./Question";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="test" element={<Question />} />
       </Routes>
     </BrowserRouter>
   );
