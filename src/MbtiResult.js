c322ca708f7d4db82b1abdd84669b5ec9490660f
bitnaleeeee <bitnaleeeee@gmail.com>
2023-11-17 15:54:11 +0900

    데이터배열생성

--- src/MbtiResult.js
+++ src/MbtiResult.js
@@ -2,23 +2,45 @@ let sortData = {};
 let mbtiData = {};
 
 const MbtiResult = () => {
-  const resultImg = [
-    ["/images/ENFJ.jpg"],
-    ["/images/ENFP.jpg"],
-    ["/images/ENTJ.jpg"],
-    ["/images/ENTP.jpg"],
-    ["/images/ESFJ.jpg"],
-    ["/images/ESTJ.jpg"],
-    ["/images/ESTP.jpg"],
-    ["/images/ENFJ.jpg"],
-    ["/images/INFJ.jpg"],
-    ["/images/INFP.jpg"],
-    ["/images/INTJ.jpg"],
-    ["/images/INTP.jpg"],
-    ["/images/ISFJ.jpg"],
-    ["/images/ISTJ.jpg"],
-    ["/images/ISTP.jpg"],
-    ["/images/INFJ.jpg"],
+  const arrResult = [
+    {
+      mbti: "ENFJ",
+      img: "/images/ENFJ.jpg",
+    },
+    {
+      mbti: "ENFP",
+      img: "/images/ENFP.jpg",
+    },
+    {
+      mbti: "ENTJ",
+      img: "/images/ENTJ.jpg",
+    },
+    {
+      mbti: "ENTP",
+      img: "/images/ENTP.jpg",
+    },
+    {
+      mbti: "ESFJ",
+      img: "/images/ESFJ.jpg",
+    },
+
+    {
+      mbti: "ESTJ",
+      img: "/images/ESTJ.jpg",
+    },
+    {
+      mbti: "ESTP",
+      img: "/images/ESTP.jpg",
+    },
+    { mbit: "ENFJ", img: "/images/ENFJ.jpg" },
+    { mbit: "INFJ", img: "/images/INFJ.jpg" },
+    { mbit: "INFP", img: "/images/INFP.jpg" },
+    { mbit: "INTJ", img: "/images/INTJ.jpg" },
+    { mbit: "INTP", img: "/images/INTP.jpg" },
+    { mbit: "ISFJ", img: "/images/ISFJ.jpg" },
+    { mbit: "ISTJ", img: "/images/ISTJ.jpg" },
+    { mbit: "ISTP", img: "/images/ISTP.jpg" },
+    { mbit: "INFJ", img: "/images/INFJ.jpg" },
   ];
   let resultData = JSON.parse(localStorage.getItem("MBTI"));
 
@@ -30,7 +52,6 @@ const MbtiResult = () => {
       Object.entries(mbtiData).sort((a, b) => b[1] - a[1])
     );
   }
-  dataFiltering(resultData);
 
   function dataProcessing(data) {
     let arr = Object.keys(data);
@@ -38,14 +59,14 @@ const MbtiResult = () => {
     let str = arr.join("");
     console.log(str);
   }
-
+  dataFiltering(resultData);
   dataProcessing(sortData);
 
   return (
     <>
       <div>
         결과페이지지
-        <img src={resultImg[0]} alt="테스트결과이미지"></img>
+        <img src={arrResult[0].img} alt="테스트결과이미지"></img>
       </div>
     </>
   );
